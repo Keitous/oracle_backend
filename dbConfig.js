b35859f9a7ee4a71be644a4b732e3a305de2adb0
Keitous <160474912+Keitous@users.noreply.github.com>
2024-09-25 12:44:24 +0000

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -2,5 +2,6 @@
 module.exports = {
   user: 'getv',
   password: 'Getv2023',
-  connectString: 'keita:1550/getv'
+  connectString: 'https://ac3b74f45b66b1c01e9483bdae1d9c5b.serveo.net/'
+  //connectString: 'keita:1550/getv'
 };
